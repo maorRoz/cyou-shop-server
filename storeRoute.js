398693360675c5db6ff42bd97f32e9d13e4d4393
maor rozevich <andory@post.bgu.ac.il>
2018-07-06 02:39:21 +0300

    made the mongo respond to insert query

--- storeRoute.js
+++ storeRoute.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const storeDB = 'mongodb://andory:maor2306@cluster0-shard-00-00-llbs8.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&serverSelectionTryOnce=false&serverSelectionTimeoutMS=15000';
-mongoose.connect(storeDB,{ useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
+const storeDB = 'mongodb://andory:maor2306@cluster0-shard-00-00-llbs8.mongodb.net:27017/ShopDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&serverSelectionTryOnce=false&serverSelectionTimeoutMS=15000';
+mongoose.connect(storeDB,{auth:{authdb:"admin"},useNewUrlParser: true});
+const dbConnection = mongoose.connection;
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 router.post('/store',function(req,res){
     console.log('got post and...');
     console.log(req.body);
+    mongoose.model('shop',new mongoose.Schema({name: String}));
+    let store = mongoose.model('shop');
+    store.create({name: "maor shop"},function(err,name){if(err){console.log(err)}});
+   // dbConnection.Shops.insert(req.body);
     res.sendStatus(200);
 });
 
